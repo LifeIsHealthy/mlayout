22869fe7d510557ac0f9cbcd77aad97ff50c5419
Manuel Reinhardt <manuel.rhdt@gmail.com>
2018-06-12 20:05:26 +0200

    Small improvements in svg_renderer

--- mathimg/src/svg_renderer.rs
+++ mathimg/src/svg_renderer.rs
@@ -1,21 +1,21 @@
-extern crate svg;
-extern crate math_render;
 extern crate freetype;
 extern crate harfbuzz_rs;
+extern crate math_render;
+extern crate svg;
 
 use std::path;
 
 use math_render::math_box::*;
 use math_render::shaper::*;
 
-use self::svg::Document;
-use self::svg::node::element::{Rectangle, Line, Group, Path};
 use self::svg::node::element::path::Data;
-use self::svg::node::{Node};
+use self::svg::node::element::{Group, Line, Path, Rectangle};
+use self::svg::node::Node;
+use self::svg::Document;
 
-use freetype::{face, Vector};
 use freetype::face::Face as FT_Face;
 use freetype::outline::Curve;
+use freetype::{face, Vector};
 
 pub struct Flags {
     pub show_ink_bounds: bool,
@@ -23,20 +23,26 @@ pub struct Flags {
     pub show_top_accent_attachment: bool,
 }
 
-pub fn render<'a, T: AsRef<path::Path>>(math_box: MathBox,
-                                        _: &HarfbuzzShaper,
-                                        font: &'a FT_Face,
-                                        flags: Flags,
-                                        out_path: T) {
+pub fn render<'a, T: AsRef<path::Path>>(
+    math_box: MathBox,
+    _: &HarfbuzzShaper,
+    font: &'a FT_Face,
+    flags: Flags,
+    out_path: T,
+) {
     let logical_extents = math_box.extents();
 
     let mut document = Document::new();
     // let mut group = Group::new();
-    document.assign("viewBox",
-                    (math_box.origin.x - 10,
-                     math_box.origin.y - math_box.extents().ascent - 10,
-                     math_box.advance_width() + 20,
-                     logical_extents.descent + logical_extents.ascent + 20));
+    document.assign(
+        "viewBox",
+        (
+            math_box.origin.x - 10,
+            math_box.origin.y - math_box.extents().ascent - 10,
+            math_box.advance_width() + 20,
+            logical_extents.descent + logical_extents.ascent + 20,
+        ),
+    );
 
     let mut italic_cor_group = Group::new()
         .set("stroke", "black")
@@ -54,33 +60,37 @@ pub fn render<'a, T: AsRef<path::Path>>(math_box: MathBox,
 
     let mut black_group = Group::new().set("fill", "black").set("stroke", "none");
 
-    generate_svg(&mut italic_cor_group,
-                 &math_box,
-                 &|group, math_box| draw_italic_correction(group, math_box));
-    generate_svg(&mut top_accent_attachment_group,
-                 &math_box,
-                 &|group, math_box| draw_top_accent_attachment(group, math_box));
-    generate_svg(&mut black_group,
-                 &math_box,
-                 &|group, math_box| draw_glyph(group, math_box, font));
-    generate_svg(&mut black_group,
-                 &math_box,
-                 &|group, math_box| draw_filled(group, math_box));
+    generate_svg(&mut italic_cor_group, &math_box, &|group, math_box| {
+        draw_italic_correction(group, math_box)
+    });
+    generate_svg(
+        &mut top_accent_attachment_group,
+        &math_box,
+        &|group, math_box| draw_top_accent_attachment(group, math_box),
+    );
+    generate_svg(&mut black_group, &math_box, &|group, math_box| {
+        draw_glyph(group, math_box, font)
+    });
+    generate_svg(&mut black_group, &math_box, &|group, math_box| {
+        draw_filled(group, math_box)
+    });
 
     if flags.show_ink_bounds {
         let mut ink_group = Group::new().set("stroke", "none").set("fill", "#FFE6E6");
-        generate_svg(&mut ink_group,
-                     &math_box,
-                     &|group, math_box| draw_ink_rect(group, math_box));
+        generate_svg(&mut ink_group, &math_box, &|group, math_box| {
+            draw_ink_rect(group, math_box)
+        });
         document.append(ink_group);
     }
 
     if flags.show_logical_bounds {
-        let mut logical_group =
-            Group::new().set("stroke", "#FF0000").set("stroke-width", 5).set("fill", "none");
-        generate_svg(&mut logical_group,
-                     &math_box,
-                     &|group, math_box| draw_logical_bounds(group, math_box));
+        let mut logical_group = Group::new()
+            .set("stroke", "#FF0000")
+            .set("stroke-width", 5)
+            .set("fill", "none");
+        generate_svg(&mut logical_group, &math_box, &|group, math_box| {
+            draw_logical_bounds(group, math_box)
+        });
         document.append(logical_group);
     }
 
@@ -94,9 +104,9 @@ pub fn render<'a, T: AsRef<path::Path>>(math_box: MathBox,
     svg::save(out_path, &document).unwrap();
 }
 
-
 fn generate_svg<'a, F>(node: &mut Group, math_box: &MathBox, func: &F)
-    where F: Fn(&mut Group, &MathBox)
+where
+    F: Fn(&mut Group, &MathBox),
 {
     let content = math_box.content();
     match *content {
@@ -125,8 +135,10 @@ fn draw_filled<'a, T: Node>(doc: &mut T, math_box: &MathBox) {
             .set("x1", math_box.origin.x)
             .set("y1", math_box.origin.y - math_box.extents().ascent)
             .set("x2", vector.x + math_box.origin.x)
-            .set("y2",
-                 math_box.origin.y - math_box.extents().ascent + vector.y)
+            .set(
+                "y2",
+                math_box.origin.y - math_box.extents().ascent + vector.y,
+            )
             .set("stroke-width", thickness)
             .set("stroke", "black")
             .set("z-index", 1);
@@ -149,15 +161,11 @@ fn draw_filled<'a, T: Node>(doc: &mut T, math_box: &MathBox) {
 
 fn draw_ink_rect<'a, T: Node>(group: &mut T, math_box: &MathBox) {
     if let MathBoxContent::Drawable(Drawable::Glyph(_)) = *math_box.content() {
-
-        let ink_bounds = math_box.bounds();
-
         let ink_rect = Rectangle::new()
-            .set("x", math_box.extents().left_side_bearing)
-            .set("y", ink_bounds.origin.y - ink_bounds.extents.ascent)
-            .set("width", ink_bounds.extents.width)
-            .set("height",
-                 ink_bounds.extents.ascent + ink_bounds.extents.descent);
+            .set("x", math_box.origin.x + math_box.extents().left_side_bearing)
+            .set("y", math_box.origin.y - math_box.extents().ascent)
+            .set("width", math_box.extents().width)
+            .set("height", math_box.extents().height());
 
         group.append(ink_rect);
     }
@@ -195,20 +203,28 @@ fn draw_italic_correction<'a, T: Node>(doc: &mut T, math_box: &MathBox) {
             return;
         }
 
-        let mut group =
-            Group::new().set("transform",
-                             format!("matrix(1 0 {:?} 1 0 0)",
-                                     -math_box.italic_correction() as f32 /
-                                     math_box.extents().height() as f32));
+        let mut group = Group::new().set(
+            "transform",
+            format!(
+                "matrix(1 0 {:?} 1 0 0)",
+                -math_box.italic_correction() as f32 / math_box.extents().height() as f32
+            ),
+        );
 
         let ink_rect = Rectangle::new()
-            .set("x",
-                 ink_bounds.origin.x + ink_bounds.extents.left_side_bearing)
+            .set(
+                "x",
+                ink_bounds.origin.x + ink_bounds.extents.left_side_bearing,
+            )
             .set("y", ink_bounds.origin.y - ink_bounds.extents.ascent)
-            .set("width",
-                 ink_bounds.extents.width - math_box.italic_correction())
-            .set("height",
-                 ink_bounds.extents.ascent + ink_bounds.extents.descent);
+            .set(
+                "width",
+                ink_bounds.extents.width - math_box.italic_correction(),
+            )
+            .set(
+                "height",
+                ink_bounds.extents.ascent + ink_bounds.extents.descent,
+            );
 
         group.append(ink_rect);
 
@@ -227,8 +243,10 @@ fn draw_top_accent_attachment<'a, T: Node>(doc: &mut T, math_box: &MathBox) {
 
 fn draw_glyph<'a, T: Node>(doc: &mut T, math_box: &MathBox, face: &FT_Face) {
     let (glyph, scale_x, scale_y) =
-        if let MathBoxContent::Drawable(Drawable::Glyph(MathGlyph { glyph_code, scale, .. })) =
-            *math_box.content() {
+        if let MathBoxContent::Drawable(Drawable::Glyph(MathGlyph {
+            glyph_code, scale, ..
+        })) = *math_box.content()
+        {
             (glyph_code, scale.as_scale_mult(), scale.as_scale_mult())
         } else {
             return;
@@ -241,12 +259,13 @@ fn draw_glyph<'a, T: Node>(doc: &mut T, math_box: &MathBox, face: &FT_Face) {
         face.load_glyph(glyph, face::NO_SCALE).unwrap();
         let outline = face.glyph().outline().expect("Glyph has no outline.");
 
-        group.assign("transform",
-                     format!("translate({:?}, {:?}) scale({:?}, {:?})",
-                             origin.x,
-                             origin.y,
-                             scale_x,
-                             -scale_y));
+        group.assign(
+            "transform",
+            format!(
+                "translate({:?}, {:?}) scale({:?}, {:?})",
+                origin.x, origin.y, scale_x, -scale_y
+            ),
+        );
 
         let mut data = Data::new();
         for contour in outline.contours_iter() {
